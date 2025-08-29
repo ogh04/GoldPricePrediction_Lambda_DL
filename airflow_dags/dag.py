5e017242c65d713e1bb9cfcf65da73b981f2be6a
GHILAN Omar <ghilanomar818@gmail.com>
2025-08-29 20:55:47 +0100

    Update dag file

--- airflow_dags/dag.py
+++ airflow_dags/dag.py
@@ -1,1 +1,44 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+from datetime import datetime
+import pendulum
 
+default_args = {
+    'start_date' pendulum.datetime(2025, 5, 7, tz=AfricaCasablanca),
+    'owner' 'omar',
+}
+
+with DAG('gold_price_dag',
+         schedule_interval='@once',
+         catchup=False,
+         default_args=default_args,
+         tags=['databricks', 'gold'],
+         description='Pipeline for loading, cleaning, training, and evaluating data'
+         ) as dag
+
+    load_data = DatabricksRunNowOperator(
+        task_id='load_data',
+        databricks_conn_id='databricks_default',
+        job_id=813702747137735
+    )
+
+    clean_data = DatabricksRunNowOperator(
+        task_id='clean_data',
+        databricks_conn_id='databricks_default',
+        job_id=478883861905674
+    )
+
+    train_data = DatabricksRunNowOperator(
+        task_id='train_data',
+        databricks_conn_id='databricks_default',
+        job_id=926272105604800
+    )
+
+    evaluate_data = DatabricksRunNowOperator(
+        task_id='evaluate_data',
+        databricks_conn_id='databricks_default',
+        job_id=936804544039854
+    )
+
+
+    load_data  clean_data  train_data  evaluate_data
